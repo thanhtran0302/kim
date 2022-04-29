ea5f99ef7c1514aee346c5de5d9008ccae31144f
Thanh TRAN <thanh.tran@agro-league.com>
2022-04-30 01:44:28 +0200

    Add AdEntity to imports

--- src/ad/ad.module.ts
+++ src/ad/ad.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdService } from './ad.service';
 import { AdController } from './ad.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdEntity } from './entities/ad.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AdEntity])],
   controllers: [AdController],
   providers: [AdService],
 })
