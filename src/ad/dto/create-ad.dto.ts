03d8009b9182d0d02a2edefc9d1dad201f241cc2
Thanh TRAN <thanh.tran@agro-league.com>
2022-05-01 01:54:08 +0200

    business phone number is mandatory when creating an ad

--- src/ad/dto/create-ad.dto.ts
+++ src/ad/dto/create-ad.dto.ts
@@ -1,13 +1,14 @@
 import { AdPaymentOptions } from '../entities/ad.entity';
 
 export class CreateAdDto {
-  title!: string;
-  description!: string;
-  streetNumber!: string;
+  title: string;
+  description: string;
+  streetNumber: string;
   street: string;
-  postalCode!: string;
-  city!: string;
-  country!: string;
+  postalCode: string;
+  city: string;
+  country: string;
   website: string;
   paymentOptions: AdPaymentOptions[];
+  businessPhoneNumber: string;
 }
