6db580e5bdcbc62735534b4fbce937aae33f0ac2
Thanh TRAN <thanh.tran@agro-league.com>
2022-08-24 21:59:47 +0200

    Implement task entity

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('MagicTodo API')
+    .setDescription('REST API')
+    .setVersion('1.0')
+    .addTag('task')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4001);
 }
 bootstrap();
--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import TimestampEntity from 'src/timestamp/timestamp.entity';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
@@ -9,18 +10,39 @@ enum PRIORITY_TYPE {
 @Entity({ name: 'task' })
 export class TaskEntity extends TimestampEntity {
   @PrimaryGeneratedColumn('uuid')
+  @ApiProperty({
+    example: 'r324e-v434f-49859f-343il',
+    description: 'Task UUID',
+  })
   id: string;
 
   @Column()
+  @ApiProperty({
+    example: 'Should clean database',
+    description: 'Task title',
+  })
   title: string;
 
   @Column()
+  @ApiProperty({
+    example: 'Clean database tables, except user table',
+    description: 'Task description',
+  })
   description: string;
 
   @Column({ name: 'priority_point' })
+  @ApiProperty({
+    example: 1,
+    description:
+      'Task priority point. Point is use as multiplier with priotyType, and dueDate',
+  })
   priorityPoint: number;
 
   @Column({ name: 'due_date', type: 'timestamp' })
+  @ApiProperty({
+    example: '10/10/2022',
+    description: "Due date, it's important for algorithm",
+  })
   dueDate: string;
 
   @Column({
